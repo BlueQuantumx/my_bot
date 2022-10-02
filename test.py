82f36cb097310590f60dc5f68d626c5dc6b8f6c4
BlueQuantum <blue_quantum@outlook.com>
2022-10-03 04:02:45 +0800

    Change to Websocket

--- test.py
+++ test.py
@@ -1,8 +1,36 @@
-from json import dump, loads
-import requests
+import asyncio
+import json
+import websockets
 
-url = "http://localhost:5700"
+IP_ADDR = "127.0.0.1"
+IP_PORT = "8080"
 
-res = loads(requests.get(url + "/get_friend_list").text)["data"]
-for i in res:
-  print(i["nickname"], i["user_id"])
+
+async def serverRecv(websocket):
+  while True:
+    res = json.loads(await websocket.recv())
+    if (res["post_type"] == "message"):
+      print(res["message_id"])
+      await websocket.send(
+          json.dumps({
+              "action": "get_msg",
+              "params": {
+                  "message_id": res["message_id"]
+              },
+          }))
+      msg = await websocket.recv()
+      print("res:", res)
+      print("msg:", msg)
+    print(res["post_type"])
+
+
+async def serverRun(websocket, path):
+  print(path)
+  await serverRecv(websocket)
+
+
+if __name__ == '__main__':
+  print("======server main begin======")
+  server = websockets.serve(serverRun, IP_ADDR, IP_PORT)
+  asyncio.get_event_loop().run_until_complete(server)
+  asyncio.get_event_loop().run_forever()
