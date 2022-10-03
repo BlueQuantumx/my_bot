2af86876e282220111b2a5421df44f4707839fa1
BlueQuantum <blue_quantum@outlook.com>
2022-10-03 20:25:25 +0800

    符合新版规范的asyncio使用方式

--- test.py
+++ test.py
@@ -14,34 +14,38 @@ def cq_code_at(user_id: int) -> str:
 async def serverRecv(websocket):
   while True:
     res: dict = json.loads(await websocket.recv())
-    print(res)
-    print('\n')
-    if (res["post_type"] == "message" and res["message_type"] == "group"):
-      msg: list = res["message"].split()
-      if (msg[0] == "/echo"):
-        echo_msg = reduce(lambda x, y: x + y, msg[1:])
-        await websocket.send(
-            json.dumps({
-                "action": "send_group_msg",
-                "params": {
-                    "group_id": res["group_id"],
-                    "message": cq_code_at(res["user_id"]) + ' ' + echo_msg
-                },
-            }))
-        await websocket.recv()  # {data}
-      print(msg)
-      print("res:", res)
-    if (res["post_type"] != "meta_event"):
-      print(res["post_type"])
+    try:
+      if (res["post_type"] == "message" and res["message_type"] == "group"):
+        msg: list = res["message"].split()
+        if (msg[0] == "/echo"):
+          echo_msg = reduce(lambda x, y: x + y, msg[1:])
+          await websocket.send(
+              json.dumps({
+                  "action": "send_group_msg",
+                  "params": {
+                      "group_id": res["group_id"],
+                      "message": cq_code_at(res["user_id"]) + ' ' + echo_msg
+                  },
+              }))
+          await websocket.recv()  # {data}
+        print(msg)
+        print("res:", res)
+      if (res["post_type"] != "meta_event"):
+        print(res["post_type"])
+    except Exception as e:
+      print("Exception: ", e)
+      print(res)
+
+
+async def main():
+  async with websockets.serve(serverRun, IP_ADDR, IP_PORT):
+    await asyncio.Future()
 
 
 async def serverRun(websocket, path):
-  print(path)
   await serverRecv(websocket)
 
 
 if __name__ == '__main__':
   print("======server main begin======")
-  server = websockets.serve(serverRun, IP_ADDR, IP_PORT)
-  asyncio.get_event_loop().run_until_complete(server)
-  asyncio.get_event_loop().run_forever()
+  asyncio.run(main())
