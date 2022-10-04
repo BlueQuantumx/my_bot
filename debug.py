3fc13e348075ffadd69cb2815727c5ea65dad7f9
BlueQuantum <blue_quantum@outlook.com>
2022-10-04 11:17:26 +0800

    Add filter to debug_print

--- debug.py
+++ debug.py
@@ -1,4 +1,9 @@
-async def debug_print(message, websocket):
-  print("")
-  print(message)
-  print("")
+message_ex_filter = ["meta_event"]
+
+
+async def debug_print(message: dict, websocket):
+  if (message.get("post_type") != None):
+    if (message_ex_filter.count(message["post_type"]) == 0):
+      print("")
+      print(message)
+      print("")
